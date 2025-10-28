ac00e4ba3919a2d7e912e98c3fdff53cad63680c
Vatsal Hariramani <vatsal@badam.ai>
2025-10-28 10:46:47 +0530

    added channels

--- src/components/VideoProvider/useTranscriptions/useTranscriptions.ts
+++ src/components/VideoProvider/useTranscriptions/useTranscriptions.ts
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
 import { Room } from 'twilio-video';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || '';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export interface TranscriptionEvent {
   participant: string;
@@ -34,6 +39,28 @@ export default function useTranscriptions(room: Room | null) {
       room.off('transcription', handleTranscription);
     };
   }, [room]);
+
+  useEffect(() => {
+    const setupRealtimeChannel = async () => {
+      await supabase.realtime.setAuth(); // Needed for Realtime Authorization
+      const changes = supabase
+        .channel(`participant_sid`, {
+          config: { private: true },
+        })
+        .on('broadcast', { event: 'INSERT' }, (payload) => console.log(payload))
+        .on('broadcast', { event: 'UPDATE' }, (payload) => console.log(payload))
+        .on('broadcast', { event: 'DELETE' }, (payload) => console.log(payload))
+        .subscribe();
+
+      return changes;
+    };
+
+    const channel = setupRealtimeChannel();
+
+    return () => {
+      channel.then(ch => ch.unsubscribe());
+    };
+  }, []);
 
   const clearTranscriptions = () => {
     setTranscriptions([]);
